4384e70f7c3453beccfe530d17f2cbc0d83a19d6
akbar.daulay <akbar.daulay@quadrant-si.id>
2024-08-29 10:05:14 +0700

    making list using dummy

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,17 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work, other }
 
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+  Category.other: Icons.devices_other,
+};
+
 class Expense {
   Expense({
-   required this.title,
-   required this.amount, 
-   required this.date,
-   required this.category,
-   })
-      : id = uuid.v4();
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
@@ -19,5 +30,7 @@ class Expense {
   final DateTime date;
   final Category category;
 
-
+ String get formattedDate {
+    return formatter.format(date);
+  }
 }
